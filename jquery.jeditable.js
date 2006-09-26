be9f4bb2bc4a43a7be78a1ec418d0c1bb5ac0e0b
Mika Tuupola <tuupola@appelsiini.net>
2006-09-26 06:01:51 +0000

    * initial commit

---
+++ jquery.jeditable.js
@@ -0,0 +1,89 @@
+/**
+* jQuery inplace editor plugin.
+*
+* Base on Dylan Verheul's editable: http://www.dyve.net/jquery/?editable
+*/
+
+$.fn.editable = function(url, options) {
+
+    var settings = {
+        url    : url,
+        name   : 'value',
+        id     : 'id',
+        type   : 'text'
+    };
+
+    if(options) {
+        $.extend(settings, options);
+    };
+
+    this.click(function(e) {
+
+        /* save this to self because this changes when scope changes */
+        var self = this;
+
+        /* prevent throwing an exeption if edit field is clicked again */
+        if (self.editing) {
+            return;
+        }
+
+        self.editing = true;
+        self.revert  = this.textContent;
+        self.innerHTML = "";
+
+        /* create the form object */
+        var f = document.createElement("form");
+
+        /*  main input element */
+        var i = document.createElement("input");
+        i.type  = "text";
+        i.name  = settings.name;
+        i.value = self.revert;
+        f.appendChild(i);
+
+        /* element containing id of element being edited*/
+        var h = document.createElement("input");
+        h.type = "hidden";
+        h.value = self.id;
+        h.name  = settings.id;
+
+        self.appendChild(f);
+        console.log(f);
+
+        i.focus();
+ 
+        console.log(this);
+        console.log(self);
+
+        $(i).keydown(function(e) {
+	    if (e.keyCode == 27) {
+                e.preventDefault();
+                reset();
+            }
+        });
+
+        $(f).submit(function(e) {
+            /* do no submit */
+            e.preventDefault(); 
+
+            /* add edited content and id of edited element to POST */           
+            var p = {};
+            p[i.name] = $(i).val();
+            p[h.name] = $(h).val();
+
+            reset();
+
+            console.log(e);
+        });
+
+        function reset() {
+            self.innerHTML = self.revert;
+            self.editing   = false;
+        };
+
+    });
+
+    $.editable = function() {
+    };
+
+}
