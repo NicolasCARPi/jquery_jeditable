38bb76233e2115a37f5795273ee092c0ffa51c7f
Mika Tuupola <tuupola@appelsiini.net>
2006-09-28 18:09:50 +0000

    * license block * ws

--- jquery.jeditable.js
+++ jquery.jeditable.js
@@ -1,21 +1,56 @@
-/**
-* jQuery inplace editor plugin.
-*
-* Based on Dylan Verheul's editable: http://www.dyve.net/jquery/?editable
-*
-* @param String  url                POST URL to send edited content
-* @param Hash    options            additional options 
-* @param String  options[name]      POST parameter name of edited content
-* @param String  options[id]        POST parameter name of edited div id
-* @param String  options[type]      text or textarea
-* @param Integer options[rows]      number of rows if using textarea
-* @param Integer options[cols]      number of columns if using textarea
-* @param String  options[postload]  POST URL to fetch content before editing
-* @param String  options[getload]   GET URL to fetch content before editing
-* @param String  options[indicator] indicator html to show when saving
-*             
-* $Id$
+/*
++-----------------------------------------------------------------------+
+| Copyright (c) 2006 Mika Tuupola, Dylan Verheul                        |
+| All rights reserved.                                                  |
+|                                                                       |
+| Redistribution and use in source and binary forms, with or without    |
+| modification, are permitted provided that the following conditions    |
+| are met:                                                              |
+|                                                                       |
+| o Redistributions of source code must retain the above copyright      |
+|   notice, this list of conditions and the following disclaimer.       |
+| o Redistributions in binary form must reproduce the above copyright   |
+|   notice, this list of conditions and the following disclaimer in the |
+|   documentation and/or other materials provided with the distribution.|
+|                                                                       |
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
+| "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
+| LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
+| A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
+| OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
+| SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
+| LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
+| DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
+| THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
+| (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
+| OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
+|                                                                       |
++-----------------------------------------------------------------------+
+| Authors: Mika Tuupola <tuupola@appelsiini.net>                        |
+|          Dylan Verheul <dylan@dyve.net>                               |
+| Version: 0.9.0                                                        |
++-----------------------------------------------------------------------+
 */
+
+/* $Id$ */
+
+/**
+  * jQuery inplace editor plugin.  
+  *
+  * Based on Dylan Verheul's editable: http://www.dyve.net/jquery/?editable
+  *
+  * @param String  url                POST URL to send edited content
+  * @param Hash    options            additional options 
+  * @param String  options[name]      POST parameter name of edited content
+  * @param String  options[id]        POST parameter name of edited div id
+  * @param String  options[type]      text or textarea
+  * @param Integer options[rows]      number of rows if using textarea
+  * @param Integer options[cols]      number of columns if using textarea
+  * @param String  options[postload]  POST URL to fetch content before editing
+  * @param String  options[getload]   GET URL to fetch content before editing
+  * @param String  options[indicator] indicator html to show when saving
+  *             
+  */
 
 $.fn.editable = function(url, options) {
 
