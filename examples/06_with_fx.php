93e5ac611a065878fcea66b21c7c2b1eb8e4096c
Mika Tuupola <tuupola@appelsiini.net>
2006-12-29 23:22:47 +0000

    * Add new example on how to use external effects.

---
+++ examples/06_with_fx.php
@@ -0,0 +1,56 @@
+<?php      
+
+/* $Id$ */      
+
+/* No hardoced URL's in examples. Just copy the folder to server. */  
+$folder = str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['PHP_SELF']);
+$url    = sprintf('http://%s%s', $_SERVER['SERVER_NAME'], $folder);
+
+?>
+<html>
+<head>
+<title>jEditable - Textile callback and tooltips demo</title>
+<script src="lib/jquery.js" type="text/javascript"></script>
+<script src="lib/jquery.jeditable.js" type="text/javascript"></script>
+<script src="lib/jquery.highlightFade.js" type="text/javascript"></script>
+</head>
+<html>
+<div class="editable_textarea" id="paragraph_3"><?php print file_get_contents($url . 'load.php?id=paragraph_3') ?></div>
+<div class="editable_textile" id="paragraph_4"><?php print file_get_contents($url . 'load.php?id=paragraph_4&renderer=textile') ?></div>
+
+<script type="text/javascript">
+// <![CDATA[
+$(document).ready(function() {
+    $(".editable_textarea").mouseover(function() { 
+        $(this).css('background-color', '#ffffd3');
+    });
+    $(".editable_textarea").mouseout(function() { 
+        $(this).css('background-color', '#fff');
+    });
+    $(".editable_textarea").editable("<?php print $url ?>save.php", { 
+        indicator : "<img src='img/indicator.gif'>",
+        type      : "textarea",
+        submit    : "OK",
+        tooltip   : "Click to edit..."
+    });
+
+    $(".editable_textile").mouseover(function() { 
+        $(this).highlightFade({end:'#ffffd3'});
+    });
+    $(".editable_textile").mouseout(function() { 
+        $(this).highlightFade({end:'#fff', speed:200});
+    });
+    $(".editable_textxile").editable("<?php print $url ?>save.php?renderer=textile", { 
+        indicator : "<img src='img/indicator.gif'>",
+        postload  : "<?php print $url ?>load.php",
+        type      : "textarea",
+        submit    : "OK",
+        cancel    : "Cancel",
+        tooltip   : "Click to edit..."
+    });
+});
+// ]]>
+</script>
+<?php include '00_footer.php' ?>
+</html>
+
--- examples/defaults.php
+++ examples/defaults.php
@@ -3,6 +3,7 @@
 /* these are just defaults for demonstrating purposes */
 
 $default['paragraph_1'] = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.';
+$default['paragraph_3'] = $default['paragraph_1'];
 
 $default['paragraph_2'] = 'Lorem ipsum dolor sit amet, consectetuer adipiscing _elit, sed diam nonummy nibh_ euismod *tincidunt ut laoreet dolore magna aliquam* erat volutpat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto.
 
@@ -15,5 +16,5 @@ $default['paragraph_2'] = 'Lorem ipsum dolor sit amet, consectetuer adipiscing _
 # Blah
 
 Ut wisi enim ad minim veniam, "quis nostrud exerci":http://www.appelsiini.net/~tuupola/javascript/jEditable/  tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto.';
-
+$default['paragraph_4'] = $default['paragraph_2'];
 ?>
