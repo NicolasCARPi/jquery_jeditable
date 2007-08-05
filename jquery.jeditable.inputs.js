32830db3e04e7a1d20acd98fd7d1e6f4573a414e
Mika Tuupola <tuupola@appelsiini.net>
2007-08-05 21:16:52 +0000

    Remove outdated intro text.

--- jquery.jeditable.inputs.js
+++ jquery.jeditable.inputs.js
@@ -1,20 +1,5 @@
 /**
-* Example custom inputs. There are four methods for a plugin. Two first are 
-* mandatory. Methods explained in order of their appearance:
-*
-* 1. Create input element. Receives jEditable settings as parameter. Inside 
-*    function variable *this* refers to original (clicked) element.
-*
-* 2. Set input element content. Receives two parameters. Input content (value)
-*    as string. Second parameter is jEditabl settings. Inside function variable
-*    *this* refers to input element.
-*
-* 3. Call before submit. This method should prepare value returned by 3rd party
-*    plugin ready for submitting. Receives jEditable settings as parameter.
-*    Inside function variable *this* refers to input element.
-*
-* 4. Attach 3rd party plugin to input field. Receives jEditable settings as 
-*    parameter. Inside function variable *this* refers to input element.
+* Example custom inputs. 
 *
 */
 
@@ -75,7 +60,7 @@ $.editable.addInputType('time', {
 		    minselect.append(option);
 		}
 		$(this).append(minselect);
-        
+		        
         /* Last create an hidden input. This is returned to plugin. It will */
         /* later hold the actual value which will be submitted to server.   */
         var hidden = $('<input type="hidden">');
