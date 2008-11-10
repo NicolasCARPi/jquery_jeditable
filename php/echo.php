406fa04a54892ffc38b0ac6c598d0348732b6347
Mika Tuupola <tuupola@appelsiini.net>
2008-11-10 14:17:20 +0000

    Avoid warning.

--- php/echo.php
+++ php/echo.php
@@ -2,7 +2,7 @@
 
 /* $Id: echo.php 117 2007-03-02 16:16:08Z tuupola $ */
 
-$renderer = $_GET['renderer'] ?  $_GET['renderer'] : $_POST['renderer'];
+$renderer = isset($_GET['renderer']) ?  $_GET['renderer'] : $_POST['renderer'];
 if ('textile' == $renderer) {
     require_once './Textile.php';
     $t = new Textile();
