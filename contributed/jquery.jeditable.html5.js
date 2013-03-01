8172ce52961c55ca419782f61281c7a0f3d85c31
Nick Freear <nfreear@yahoo.co.uk>
2013-03-01 15:17:21 +0000

    Issue #84, jquery.jeditable.html5 is a Javascript plugin for Jeditable
    * It adds HTML5 form input types - eg. type="number"..
    * ..Form validation constraints - eg. required, pattern= ..
    * And, improves keyboard accessibility using WAI-ARIA roles etc.

---
+++ contributed/jquery.jeditable.html5.js
@@ -0,0 +1,129 @@
+/*
+ * HTML5 and accessibility/WAI-ARIA support for Jeditable.
+ *
+ * Copyright (c) 2013 Nick Freear, The Open University.
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+
+/*jslint browser: true, indent: 4 */
+
+(function ($) {
+
+    'use strict';
+
+    // Keyboard accessibility/WAI-ARIA - allow users to navigate to an editable element using TAB/Shift+TAB.
+    $.fn.editableAriaShim = function () {
+        //$.log('WAI-ARIA shim..');
+
+        $(this).attr({
+            role: 'button',
+            tabindex: 0
+            //, title: 'Editable' -- See 'tooltip' below.
+            //, 'aria-label':''
+        });
+        return this; //<--jquery object - chaining.
+    };
+
+
+    // Keyboard accessibility - use mouse click OR press any key to enable editing.
+    $.fn.editable.defaults.event = 'click.editable keydown.editable';
+
+    // Accessibility - there should be a default value for the title/ tooltip.
+    $.fn.editable.defaults.tooltip = 'Click to edit'; //$.fn.editable.defaults.placeholder
+
+
+    $.editable.addInputType('html5_text', {
+        element: function (settings, original) {
+            var input = $('<input />').attr({
+                // required: settings.required ? 'required' : '', -- See below.
+                // autofocus -- Not relevant for Jeditable.
+                // disabled -- Not relevant.
+                // readonly -- Not relevant.
+                //autocomplete: settings.autocomplete, -- Todo.
+                inputmode: settings.input.mode,
+                list: settings.list,
+                maxlength: settings.maxlength,
+                pattern: settings.pattern,
+                placeholder: settings.html5_placeholder, // Parameter name conflict - add 'html5_'.
+                title: settings.html5_error_text,
+                type: 'text'
+            });
+            if (settings.required) { input.attr('required', ''); } // Is this relevant to Jeditable? Probably.
+
+            $(this).append(input);
+            return input;
+        }
+    });
+
+$.editable.addInputType('unsigned_integer', {
+  element: function (settings, original) {
+    var input = $('<input type="number" min="0" max="10" step="1">');
+    $(this).append(input);
+    return input;
+  }
+});
+
+$.editable.addInputType('number', {
+  element: function (settings, original) {
+    var input = $('<input />').attr({
+        maxlength: settings.maxlength,
+        //pattern: settings.pattern, -- Does not apply to 'number' (but may be useful if 'number' is not supported).
+        placeholder: settings.html5_placeholder,
+        min : settings.min,
+        max : settings.max,
+        step: settings.step,
+        title: settings.html5_error_text,
+        type: 'number'
+    });
+    $(this).append(input);
+    return input;
+  }
+});
+
+/*$.editable.addInputType('range', {
+  element: function (settings, original) {
+    var input = $('<input />').attr({
+        maxlength: settings.maxlength,
+        pattern: settings.pattern,
+        placeholder: settings.html5_placeholder,
+        min : settings.min,
+        max : settings.max,
+        step: settings.step,
+        type: 'range'
+    });
+    $(this).append(input);
+    return input;
+  }
+});*/
+
+$.editable.addInputType('email', {
+  element: function (settings, original) {
+    var input = $('<input />').attr({
+        // pattern -- Not useful.
+        maxlength: settings.maxlength,
+        placeholder: settings.html5_placeholder,
+        type: 'email'
+    });
+    $(this).append(input);
+    return input;
+  }
+});
+
+$.editable.addInputType('url', {
+  element: function (settings, original) {
+    var input = $('<input />').attr({
+        maxlength: settings.maxlength,
+        pattern: settings.pattern,
+        placeholder: settings.html5_placeholder,
+        title: settings.html5_error_text,
+        type: 'url'
+    });
+    $(this).append(input);
+    return input;
+  }
+});
+
+
+})(jQuery);
