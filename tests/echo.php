45ec18f7dd3b4d773fac8c1e71894f18a92452ab
Nick Freear <nfreear@yahoo.co.uk>
2013-03-14 15:35:28 +0000

    Bug #84, Improve and fix error handling of PHP scripts.

--- tests/echo.php
+++ tests/echo.php
@@ -2,7 +2,7 @@
 
 /* $Id: echo.php 117 2007-03-02 16:16:08Z tuupola $ */
 
-$renderer = isset($_GET['renderer']) ?  $_GET['renderer'] : $_POST['renderer'];
+$renderer = isset($_GET['renderer']) ?  $_GET['renderer'] : (isset($_POST['renderer']) ? $_POST['renderer'] : NULL);
 if ('textile' == $renderer) {
     require_once './Textile.php';
     $t = new Textile();
