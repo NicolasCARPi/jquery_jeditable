362f182c5672617884fd08d9b3bd4bd0039b3908
Igor Zinovyev <zinigor@gmail.com>
2013-08-28 19:07:42 +0400

    Fixed the container height and width bug.

--- jquery.jeditable.js
+++ jquery.jeditable.js
@@ -452,8 +452,8 @@
             text: {
                 element : function(settings, original) {
                     var input = $('<input />');
-                    if (settings.width  != 'none') { input.attr('width', settings.width);  }
-                    if (settings.height != 'none') { input.attr('height', settings.height); }
+                    if (settings.width  != 'none') { input.width(settings.width);  }
+                    if (settings.height != 'none') { input.height(settings.height); }
                     /* https://bugzilla.mozilla.org/show_bug.cgi?id=236791 */
                     //input[0].setAttribute('autocomplete','off');
                     input.attr('autocomplete','off');
