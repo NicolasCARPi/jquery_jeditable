b5d32e2bd2ab35fd5f7884879575b10e279ce0ee
donarus <tadeas@palusga.cz>
2013-11-02 21:31:47 +0100

    added "type" attribute to editable "text" input elements

--- jquery.jeditable.js
+++ jquery.jeditable.js
@@ -457,6 +457,7 @@
                     /* https://bugzilla.mozilla.org/show_bug.cgi?id=236791 */
                     //input[0].setAttribute('autocomplete','off');
                     input.attr('autocomplete','off');
+                    input.attr('type','text');
                     $(this).append(input);
                     return(input);
                 }
