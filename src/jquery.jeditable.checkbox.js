4b4b3a5cda8490120835020d21fe9b192ff6ac18
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-12-19 18:06:42 +0100

    add checkbox type. by @oneslash and @pushpinderbagga. (#52)

--- src/jquery.jeditable.checkbox.js
+++ src/jquery.jeditable.checkbox.js
@@ -37,5 +37,16 @@ $.editable.addInputType("checkbox", {
 
 		var value = $(input).attr("checked") ? 'on' : 'off';
 		$(input).val(value);
-	}
+	},
+
+    submit: function (settings, original) {
+        var value;
+        var input = $(':input:first', this);
+        if (input.attr('checked')) {
+            value = '1';
+        } else {
+            value = '0';
+        }
+        $("input", this).val(value);
+    }
 });
