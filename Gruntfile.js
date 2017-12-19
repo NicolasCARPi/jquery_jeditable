f9a2e227064b468436a2fbaac5b1f3e70b4cf86c
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-12-19 11:04:27 +0100

    use src and dist, use grunt for minify

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/**
+ * Gruntfile.js
+ *
+ * Run 'grunt' in shell to minify javascript
+ *
+ */
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+          banner: '/*! <%= pkg.name %> <%= pkg.homepage %> */\n',
+        mangle: false
+      },
+      dist: {
+        files: {
+          'dist/jquery.jeditable.min.js': 'src/jquery.jeditable.js',
+          'dist/jquery.jeditable.ajaxupload.min.js': 'src/jquery.jeditable.ajaxupload.js',
+          'dist/jquery.jeditable.autogrow.min.js': 'src/jquery.jeditable.autogrow.js',
+          'dist/jquery.jeditable.charcounter.min.js': 'src/jquery.jeditable.charcounter.js',
+          'dist/jquery.jeditable.datepicker.min.js': 'src/jquery.jeditable.datepicker.js',
+          'dist/jquery.jeditable.masked.min.js': 'src/jquery.jeditable.masked.js',
+          'dist/jquery.jeditable.time.min.js': 'src/jquery.jeditable.time.js',
+          'dist/jquery.jeditable.timepicker.min.js': 'src/jquery.jeditable.timepicker.js',
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', 'uglify');
+
+};
