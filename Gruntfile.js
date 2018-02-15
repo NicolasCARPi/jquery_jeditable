5f6b3c0449cdc4c031eb493a9dff73f4cd6146da
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-15 14:25:57 +0100

    use double quotes

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,26 +7,26 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     uglify: {
       options: {
-          banner: '/*! <%= pkg.name %> <%= pkg.homepage %> */\n',
+          banner: "/*! <%= pkg.name %> <%= pkg.homepage %> */\n",
         mangle: false
       },
       dist: {
         files: {
-          'dist/jquery.jeditable.min.js': 'src/jquery.jeditable.js',
-          'dist/jquery.jeditable.autogrow.min.js': 'src/jquery.jeditable.autogrow.js',
-          'dist/jquery.jeditable.charcounter.min.js': 'src/jquery.jeditable.charcounter.js',
-          'dist/jquery.jeditable.datepicker.min.js': 'src/jquery.jeditable.datepicker.js',
-          'dist/jquery.jeditable.masked.min.js': 'src/jquery.jeditable.masked.js',
-          'dist/jquery.jeditable.time.min.js': 'src/jquery.jeditable.time.js',
+          "dist/jquery.jeditable.min.js": "src/jquery.jeditable.js",
+          "dist/jquery.jeditable.autogrow.min.js": "src/jquery.jeditable.autogrow.js",
+          "dist/jquery.jeditable.charcounter.min.js": "src/jquery.jeditable.charcounter.js",
+          "dist/jquery.jeditable.datepicker.min.js": "src/jquery.jeditable.datepicker.js",
+          "dist/jquery.jeditable.masked.min.js": "src/jquery.jeditable.masked.js",
+          "dist/jquery.jeditable.time.min.js": "src/jquery.jeditable.time.js",
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify-es');
-  grunt.registerTask('default', 'uglify');
+  grunt.loadNpmTasks("grunt-contrib-uglify-es");
+  grunt.registerTask("default", "uglify");
 
 };
