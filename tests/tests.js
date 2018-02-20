1fbc571d80aa417bbf96beffa0b452a3f549399e
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-20 18:43:13 +0100

    write first real test

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,4 @@
 /* jquery-jeditable tests */
-QUnit.test( 'hello test', function( assert ) {
-    assert.ok( 1 == '1', 'Passed!' );
+QUnit.test('Registration', function(assert) {
+    assert.ok($.fn.editable, 'registered as a jQuery plugin');
 });
