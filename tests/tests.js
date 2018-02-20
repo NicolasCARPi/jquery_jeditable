80809dcadc03143dd0a66da48c3b5e078aba3b42
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-20 22:02:48 +0100

    add more tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,15 @@
 /* jquery-jeditable tests */
+var elem = $('#qunit-fixture');
+
 QUnit.test('Registration', function(assert) {
     assert.ok($.fn.editable, 'registered as a jQuery plugin');
+});
+QUnit.test('Chainability', function(assert) {
+    assert.ok(elem.editable().addClass('testing'), 'can be chained');
+    assert.ok(elem.hasClass('testing'), 'successfully chained');
+});
+
+QUnit.test('ARIA attributes', function(assert) {
+    elem.editable().editableAriaShim();
+    assert.ok(elem.is('[role="button"]'), 'added role');
 });
